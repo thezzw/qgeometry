90c2787873309755f71036b37bf7cf1c944679cb
元周 <zhoubei.zhou@alibaba-inc.com>
2025-12-09 20:58:49 +0800

    refactor: QCircle {pos: QVec2 -> center: QPoint}

--- src/shape/circle.rs
+++ src/shape/circle.rs
@@ -5,21 +5,21 @@ use super::{ QPoint, QBbox, QPolygon, QShapeCommon, QShapeType };
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct QCircle {
-    pos: QVec2,
+    center: QPoint,
     radius: Q64,
 }
 
 impl QCircle {
-    pub fn new(pos: QVec2, radius: Q64) -> Self {
+    pub fn new(center: QPoint, radius: Q64) -> Self {
         assert!(radius > Q64::ZERO, "[QCircle::new] radius({radius:?}) should be larger than zero.");
         Self {
-            pos,
+            center,
             radius,
         }
     }
 
-    pub fn pos(&self) -> QVec2 {
-        self.pos
+    pub fn center(&self) -> QPoint {
+        self.center
     }
 
     pub fn radius(&self) -> Q64 {
@@ -35,8 +35,8 @@ impl QShapeCommon for QCircle {
         for i in 0..num_points {
             let angle: Q64 = q64!(i) / q64!(num_points) * Q64::TAU;
             let (sin, cos) = angle.sin_cos();
-            let x = self.pos.x.saturating_add(self.radius.saturating_mul(cos));
-            let y = self.pos.y.saturating_add(self.radius.saturating_mul(sin));
+            let x = self.center.x().saturating_add(self.radius.saturating_mul(cos));
+            let y = self.center.y().saturating_add(self.radius.saturating_mul(sin));
             points.push(QPoint::new(QVec2::new(x, y)));
         }
 
@@ -44,11 +44,11 @@ impl QShapeCommon for QCircle {
     }
 
     fn get_bbox(&self) -> QBbox {
-        QBbox::new_from_parts(self.pos.saturating_sub_num(self.radius), self.pos.saturating_add_num(self.radius))
+        QBbox::new_from_parts(self.center.pos().saturating_sub_num(self.radius), self.center.pos().saturating_add_num(self.radius))
     }
 
     fn get_centroid(&self) -> QPoint {
-        QPoint::new(self.pos)
+        self.center
     }
 
     fn get_shape_type(&self) -> QShapeType {
@@ -56,7 +56,7 @@ impl QShapeCommon for QCircle {
     }
 
     fn is_point_inside(&self, point: &QPoint) -> bool {
-        self.pos.distance_squared(point.pos()) <= self.radius.saturating_mul(self.radius)
+        self.center.pos().distance_squared(point.pos()) <= self.radius.saturating_mul(self.radius)
     }
 
     fn is_collide(&self, other: &impl QShapeCommon) -> bool {
